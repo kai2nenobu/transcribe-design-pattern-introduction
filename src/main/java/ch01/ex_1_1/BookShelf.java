746b28d89b98b7ddec4a86e8ac3c8845bc158507
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-11-08 17:50:49 +0900

    練習問題1-1用にサンプルプログラムを複製する

---
+++ src/main/java/ch01/ex_1_1/BookShelf.java
@@ -0,0 +1,22 @@
+package ch01.ex_1_1;
+
+public class BookShelf {
+    private Book[] books;
+    private int last = 0;
+    public BookShelf(int maxsize) {
+        this.books = new Book[maxsize];
+    }
+    public Book getBookAt(int index) {
+        return books[index];
+    }
+    public void appendBook(Book book) {
+        this.books[last] = book;
+        last++;
+    }
+    public int getLength() {
+        return last;
+    }
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
---
+++ src/main/java/ch01/ex_1_1/Iterator.java
@@ -0,0 +1,6 @@
+package ch01.ex_1_1;
+
+public interface Iterator {
+    public abstract boolean hasNext();
+    public abstract Object next();
+}
